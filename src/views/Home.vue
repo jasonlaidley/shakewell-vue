08db840c1e93369344b589696535acf8ff0466f7
jasonlaidleyboohoo <jason.laidley@boohoo.com>
2022-02-04 16:00:07 +0000

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,18 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  mounted() {
+    console.log('hello');
+    axios.get('http://localhost/shakes').then((response) => {
+      console.log(response);
+    });
   },
 };
 </script>

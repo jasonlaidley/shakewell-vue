2e5b18af158e326a10cff9fc18e8780611ff5d54
jasonlaidley <devonne_laidley@yahoo.co.uk>
2022-02-07 22:05:34 +0000

    update

---
+++ src/views/Shake.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='home'>
+    <img alt='Vue logo' src='../assets/logo.png' />
+    <ShakeVideo />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ShakeVideo from '@/components/ShakeVideo.vue';
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      timerCount: 30,
+    };
+  },
+  name: 'Home',
+  components: {
+    ShakeVideo,
+  },
+  mounted() {
+    console.log('hello');
+    axios.get('http://localhost/shakes').then((response) => {
+      console.log(response);
+    });
+    document.getElementById('videoFront1').play();
+  },
+};
+</script>

18edfcb89c5df382c4700848a4509a8724406c70
jasonlaidley <devonne_laidley@yahoo.co.uk>
2022-02-24 10:17:06 +0000

    start vimeo

--- src/views/Shake.vue
+++ src/views/Shake.vue
@@ -1,10 +1,12 @@
 <template>
   <div class='home'>
-    <img alt='Vue logo' src='../assets/logo.png' />
     <ShakeVideo />
+    <iframe src="https://player.vimeo.com/video/678926274?h={hash_parameter}" width="{video_width}" height="{video_height}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <div id="vim1"></div>
   </div>
 </template>
 
+<script src="https://player.vimeo.com/api/player.js"></script>
 <script>
 // @ is an alias to /src
 import ShakeVideo from '@/components/ShakeVideo.vue';
@@ -21,11 +23,19 @@ export default {
     ShakeVideo,
   },
   mounted() {
-    console.log('hello');
     axios.get('http://localhost/shakes').then((response) => {
       console.log(response);
     });
     document.getElementById('videoFront1').play();
+    //Vimeo
+    const options01 = {
+      id: 678926274,
+      width: 500
+    };
+    const video01Player = new Vimeo.Player('vim1', options01);
+    video01Player.on('play', function() {
+      console.log('Played the first video');
+    });
   },
 };
 </script>
